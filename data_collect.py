e95194923cfdfe4d887e41186273005dfec07c14
shshinc <ksk031300@naver.com>
2023-05-01 14:20:55 +0900

    페이지 스크롤

--- data_collect.py
+++ data_collect.py
@@ -5,6 +5,26 @@ from selenium.webdriver.common.keys import Keys
 import time
 import urllib.request
 
+def selenium_scroll_option():
+    SCROLL_PAUSE_SEC = 3
+
+    # 스크롤 높이 가져옴
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # 끝까지 스크롤 다운
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # 1초 대기
+        time.sleep(SCROLL_PAUSE_SEC)
+
+        # 스크롤 다운 후 스크롤 높이 다시 가져옴
+        new_height = driver.execute_script("return document.body.scrollHeight")
+
+        if new_height == last_height:
+            break
+        last_height = new_height
+
 chrome_options = webdriver.ChromeOptions()
 base_url = "https://www.google.co.kr/imghp?hl=ko"
 word = ['cup', 'tumbler', "vacuum"]
